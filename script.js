68b965533dda9a77aa00fe1d0ecc6f4955b52043
Carol B <carolb@bu.edu>
2023-02-16 13:26:05 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    loop: false,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 4,
+        },
+    },
+});
